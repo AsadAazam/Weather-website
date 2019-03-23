469ed1d792124a65e1825c896e7ced156ba2a17a
Asad Aazam <asadaazamcse16@gmail.com>
2019-03-23 17:21:10 +0530

    removing unnecessary console.log statements

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!!')
+
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
 //        console.log(data)
@@ -16,19 +16,17 @@ const messageTwo=document.querySelector('#message-2')
 webForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
-    console.log(location)
+   
     messageOne.textContent='Loading please wait'
     messageTwo.textContent=''
     fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {
-              console.log('Error=',data.error)
              messageOne.textContent=data.error
         }
         else{
-        console.log("location",data.location)
-        console.log("forcaste",data.forecast)
+        
         messageOne.textContent=data.location
         messageTwo.textContent=data.forecast
         }
